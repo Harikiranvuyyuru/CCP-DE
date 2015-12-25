706308c74661d81ecfb0722068c05db6955a57c1
SudarshanRaoP <sudarshan.visham188@gmail.com>
2015-12-25 11:49:57 +0530

    Comments

--- Cruch/crunch-wordcount/src/main/java/com/example/crunch/Tokenizer.java
+++ Cruch/crunch-wordcount/src/main/java/com/example/crunch/Tokenizer.java
@@ -5,12 +5,12 @@ import org.apache.crunch.Emitter;
 
 @SuppressWarnings("serial")
 public class Tokenizer extends DoFn<String, String>{
-	
+	//Reads strings and emits strings	
 	@Override
 	public void process(String line, Emitter<String> emitter){
-		
+		//Split lines into words
 		for (String word: line.split("\\s+")){
-			
+			//emit words
 			emitter.emit(word);
 		}
 	}
