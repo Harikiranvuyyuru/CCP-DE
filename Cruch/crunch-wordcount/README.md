e4f23b23aa9e3d37c3cdcbebd90595cf67774050
SudarshanRaoP <sudarshan.visham188@gmail.com>
2015-12-25 11:42:13 +0530

    First Crunch Program

---
+++ Cruch/crunch-wordcount/README.md
@@ -0,0 +1,3 @@
+##Crunch Word Count Program
+
+#### Usage : hadoop jar crunch-wordcount-0.1.0-SNAPSHOT-jar-with-dependencies.jar com.example.crunch.WordCount <input> <output>
--- Cruch/crunch-wordcount/src/main/java/com/example/crunch/WordCount.java
+++ Cruch/crunch-wordcount/src/main/java/com/example/crunch/WordCount.java
@@ -1,10 +1,8 @@
 package com.example.crunch;
 
-
 import org.apache.crunch.PCollection;
 import org.apache.crunch.Pipeline;
 import org.apache.crunch.PipelineResult;
-//import org.apache.crunch.impl.mem.MemPipeline;
 import org.apache.crunch.impl.mr.MRPipeline;
 import org.apache.crunch.io.To;
 import org.apache.crunch.types.writable.Writables;
@@ -16,7 +14,7 @@ import org.apache.hadoop.util.ToolRunner;
 public class WordCount extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
-		
+		//Check for arguments
 		if (args.length != 2) {
 			System.err.println("This program expects 2 arguments."
 					+ "\n\tUsage: WordCount <input> <output>");
@@ -26,15 +24,18 @@ public class WordCount extends Configured implements Tool {
 	}
 	
 	public int run(String[] args) throws Exception {
+		
+		//Create a Pipeline
 		Pipeline wcpipeline = new MRPipeline(WordCount.class, getConf());
+		// Read lines from a textfile
 		PCollection<String> lines = wcpipeline.readTextFile(args[0]);
-		
+		//Split lines into words
 		PCollection<String> words = lines.parallelDo(new Tokenizer(), Writables.strings());
-		
+		//Associate a count to each word in the Pcollection<String> words -> PTable<String, Integer>
 		words.count().write(To.textFile(args[1]));
-		
+		//Execute using done() method
 		PipelineResult result = wcpipeline.done();
-		
+		//Return exit code
 		return result.succeeded() ? 0 : 1 ;
 	}
 }
