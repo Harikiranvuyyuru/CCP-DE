3fc293f70a84f1cd3f601d85fa2302b01fd77eb0
SudarshanRaoP <sudarshan.visham188@gmail.com>
2015-12-30 12:34:30 +0530

    New Datetime functions

---
+++ Hive/hive-udfs/src/main/java/com/example/hive/Now.java
@@ -0,0 +1,20 @@
+package com.example.hive;
+
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class Now extends UDF {
+
+	private static Date dt = new Date();
+	Text _now = new Text();
+	public Text evaluate(){
+		
+		_now.set(DateFormatUtils.format(dt, "MM/dd/yy HH:mm:ss"));
+		
+		return _now;
+	}
+}
+
