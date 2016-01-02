36d6784558b4f4729ef0c5246dc72b77409febf7
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-01-02 14:35:20 +0530

    fixed bugs

--- Flume/flume-serializer/src/main/java/com/example/flume/serializer/WebLogAvroEventSerializer.java
+++ Flume/flume-serializer/src/main/java/com/example/flume/serializer/WebLogAvroEventSerializer.java
@@ -22,7 +22,11 @@ import org.slf4j.LoggerFactory;
 public class WebLogAvroEventSerializer extends AbstractAvroEventSerializer<WebLogEvent> {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebLogAvroEventSerializer.class);
-	private static final File schemaFile = new File("src/main/resources/weblog_event.avsc");
+	/*
+	*	Note: schemaFile have to be some in the local filesystem
+	*/
+
+	private static final File schemaFile = new File("/usr/lib/flume-ng/resources/weblog_event.avsc");
 	private final OutputStream out;
 	private final Schema schema;
 	private final String regex_log;
@@ -30,7 +34,7 @@ public class WebLogAvroEventSerializer extends AbstractAvroEventSerializer<WebLo
 	public WebLogAvroEventSerializer(OutputStream out) throws IOException {
 		this.out = out;
 		this.schema = new Schema.Parser().parse(schemaFile);
-		this.regex_log = "^(\\d+\\.\\d+\\.\\d+\\.\\d+)\\s(-|.*)\\s(-|.*)\\s\\[(.*)\\s\\-\\d{4}\\]\\s\"(\\w{3})\\s(.*)\\s(.*)\"\\s(\\d+)\\s(\\d+)\\s\"(-|.*)\"\\s\"(-|.*)$";
+		this.regex_log = "^(.*)\\s(-|.*)\\s(-|.*)\\s\\[(.*)\\s\\-\\d{4}\\]\\s\"(\\w{3})\\s(.*)\\s(.*)\"\\s(\\d+)\\s(\\d+)\\s\"(-|.*)\"\\s\"(-|.*)$";
 	}
 
 	@Override
@@ -56,25 +60,25 @@ public class WebLogAvroEventSerializer extends AbstractAvroEventSerializer<WebLo
 
 		if (m.find()){
 			wle.setClientIp(m.group(1));
-			wle.setClientId((m.group(2) == "-") ? null : m.group(2));
-			wle.setUserId((m.group(3) == "-") ? null : m.group(3)) ;
+			wle.setClientId(m.group(2).equals("-") ? null : m.group(2));
+			wle.setUserId(m.group(3).equals("-") ? null : m.group(3)) ;
 			wle.setTimeStamp(m.group(4));
 			//http request type
-			if (m.group(5) == "GET"){
+			if (m.group(5).equals("GET")){
 				wle.setRequestType(Request.GET);
 			}
-			else if (m.group(5) == "POST") {
+			else if (m.group(5).equals("POST")) {
 				wle.setRequestType(Request.POST);
 			}
 			else { 
-				logger.warn("Unknown Request Type : %s", m.group(5)); 
+				logger.warn("Unknown Request Type : " + m.group(5)); 
 			}
 			wle.setRequestPage(m.group(6));
 			wle.setHttpProtocol(m.group(7));
 			wle.setResponseCode(Integer.parseInt(m.group(8)));
 			wle.setResponseSize(Integer.parseInt(m.group(9)));
-			wle.setReferrer((m.group(10) == "-") ? null : m.group(10));
-			wle.setUserAgent((m.group(11) == "-") ? null : m.group(11));
+			wle.setReferrer(m.group(10).equals("-") ? null : m.group(10));
+			wle.setUserAgent(m.group(11).equals("-") ? null : m.group(11));
 		}
 		else {
 			logger.error("Unknown log format.");
