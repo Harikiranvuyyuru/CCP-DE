59fe7b21db449d3fde191a2df9601faca508a1d4
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-01-11 12:51:44 +0530

    Spark App

---
+++ Spark/SimpleApp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local[2]").setAppName("Simple App")
+sc = SparkContext(conf=conf)
+
+data = sc.parallelize([1, 2, 3, 4])
+
+total = data.reduce(lambda x, y: x + y)
+
+print total
