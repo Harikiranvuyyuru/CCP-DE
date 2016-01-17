3ea6138b115fc6f67c54f05c33b1d0029cc6789c
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-01-17 11:28:04 +0530

    renamed

---
+++ Spark/simpleapp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from pyspark import SparkConf, SparkContext
+"""
+	Usage: ./spark-submit SimpleApp.py
+"""
+conf = SparkConf().setMaster("local[2]").setAppName("Simple App")
+sc = SparkContext(conf=conf)
+
+data = sc.parallelize([1, 2, 3, 4])
+
+total = data.reduce(lambda x, y: x + y)
+
+print total
+
+sc.stop()
