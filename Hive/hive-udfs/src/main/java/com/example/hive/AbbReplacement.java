3d2eaffb343f823c6fd867c3c07ffa0a75c62433
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-02-29 19:05:04 +0530

    Improved

--- Hive/hive-udfs/src/main/java/com/example/hive/AbbReplacement.java
+++ Hive/hive-udfs/src/main/java/com/example/hive/AbbReplacement.java
@@ -2,31 +2,39 @@ package com.example.hive;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 
+
 public class AbbReplacement extends UDF {
 	
-	public static Text evaluate(Text s){
+	private Map<String, String> abbrmap;
+	private Set<String> abbrkeys;
+	
+	public AbbReplacement(){
+		this.abbrmap = new HashMap<String, String>();
+		abbrmap.put("Ft", "Fort");
+		abbrmap.put("Pk", "Park");
+		abbrmap.put("Mtn", "Mountain");
+		
+		this.abbrkeys = abbrmap.keySet();
+	}
+	
+	public Text evaluate(Text s){
 		
 		Text out = new Text();
 		
-		Map<String, String> abb_ext = new HashMap<String, String>();
-		
-		abb_ext.put("Ft", "Fort");
-		abb_ext.put("Pk", "Park");
-		abb_ext.put("Mtn", "Mountain");
-		
 		String in = s.toString();
 		
-		for(String abb: abb_ext.keySet()){
+		for(String abb: abbrkeys){
 			Pattern p = Pattern.compile(abb);
 			Matcher m = p.matcher(in);
 			if (m.find()){
-				String str_out = in.replaceAll(abb, abb_ext.get(abb));
+				String str_out = in.replaceAll(abb, abbrmap.get(abb));
 				out.set(str_out);
 				return out;
 			}
@@ -35,3 +43,4 @@ public class AbbReplacement extends UDF {
 		return s;
 	}	
 }
+
