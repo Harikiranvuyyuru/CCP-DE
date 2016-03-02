0b8b0e548bb6f7d8f374acebfd0425a6790d3465
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-03-02 14:32:23 +0530

    transform

---
+++ Hive/scripts/transform.sql
@@ -0,0 +1,30 @@
+--Usage: hive -f transform.sql --hivevar fields --hivevar column=dt --hivevar pyfile=dateformatter.py --hivevar table=tbl
+
+-- dateformatter.py:
+--	#!/usr/bin/env python
+
+--	from dateutil import parser
+--	import sys
+
+--	for s in sys.stdin:
+--	    dt = parser.parse(s)
+--	    print(dt.strftime("%D"))
+
+--	------------
+--	|dt 		|
+--	|-----------|
+--	|1/11/67	|
+--	|-----------|
+--	|10-06-1998	|
+--	-------------
+
+SELECT ${hivevar:fields}, TRANSFORM({hivevar:column})
+USING 'python ${hivevar:pyfile}' FROM ${hivevar:table};
+
+--	------------
+--	|_c0		|
+--	|-----------|
+--	|01/11/67	|
+--	|-----------|
+--	|10/06/98	|
+--	-------------
